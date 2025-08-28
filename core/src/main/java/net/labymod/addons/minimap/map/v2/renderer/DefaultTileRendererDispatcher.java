11f5b88fa408afd0c0347d7841ebad6a89cdaa19
Christian <r0bby@gmx.net>
2025-08-28 10:40:17 +0200

    Disable `EntityTileRenderer` registration in `DefaultTileRendererDispatcher`.

--- core/src/main/java/net/labymod/addons/minimap/map/v2/renderer/DefaultTileRendererDispatcher.java
+++ core/src/main/java/net/labymod/addons/minimap/map/v2/renderer/DefaultTileRendererDispatcher.java
@@ -23,7 +23,7 @@ public class DefaultTileRendererDispatcher implements TileRendererDispatcher {
     this.renderers = new ArrayList<>();
 
     this.register(PlayerTileRenderer::new);
-    this.register(EntityTileRenderer::new);
+    //this.register(EntityTileRenderer::new);
   }
 
   @Subscribe
