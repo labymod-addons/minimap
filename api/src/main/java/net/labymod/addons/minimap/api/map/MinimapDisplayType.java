10cb787ee887a822c3acd2e87da3c49864aa4215
Christian <r0bby@gmx.net>
2025-09-02 17:54:29 +0200

    Add new minimap display types: `ROUND_CLASSIC`, `ROUND_METAL_GOLD`, `ROUND_SIMPLE_CHROME`, `ROUND_SIMPLE_METAL`, `ROUND_WOOD`, and `SQUARE_PARCHMENT`. Update enum, add themed textures, and extend configuration handling.

--- api/src/main/java/net/labymod/addons/minimap/api/map/MinimapDisplayType.java
+++ api/src/main/java/net/labymod/addons/minimap/api/map/MinimapDisplayType.java
@@ -19,10 +19,23 @@ public enum MinimapDisplayType {
       },
       true
   ),
+  ROUND_CLASSIC(
+      Stage.AFTER_TEXTURE,
+      Icon.texture(Util.newThemeLocation("overlay/round_classic")),
+      (context, radius) -> {
+        context.canvas().submitCircle(
+            radius, radius,
+            radius - Util.BORDER_PADDING,
+            Integer.MAX_VALUE
+        );
+      },
+      true
+  ),
   ROUND_FUTURE_WIDE(
       Stage.AFTER_TEXTURE,
       Icon.texture(Util.newThemeLocation("overlay/round_future_wide")),
-      (context, radius) -> {
+      (context, radius) ->
+      {
         context.canvas().submitCircle(
             radius, radius,
             radius - Util.BORDER_PADDING,
@@ -34,7 +47,8 @@ public enum MinimapDisplayType {
   ROUND_FUTURE_THIN(
       Stage.AFTER_TEXTURE,
       Icon.texture(Util.newThemeLocation("overlay/round_future_thin")),
-      (context, radius) -> {
+      (context, radius) ->
+      {
         context.canvas().submitCircle(
             radius, radius,
             radius - Util.BORDER_PADDING + 1,
@@ -43,10 +57,72 @@ public enum MinimapDisplayType {
       },
       true
   ),
+  ROUND_METAL_GOLD(
+      Stage.AFTER_TEXTURE,
+      Icon.texture(Util.newThemeLocation("overlay/round_metal_gold")),
+      (context, radius) -> {
+        context.canvas().submitCircle(
+            radius, radius,
+            radius - Util.BORDER_PADDING,
+            Integer.MAX_VALUE
+        );
+      },
+      true
+  ),
+  ROUND_SIMPLE_CHROME(
+      Stage.AFTER_TEXTURE,
+      Icon.texture(Util.newThemeLocation("overlay/round_simple_chrome")),
+      (context, radius) -> {
+        context.canvas().submitCircle(
+            radius, radius,
+            radius + 1,
+            Integer.MAX_VALUE
+        );
+      },
+      true
+  ),
+  ROUND_SIMPLE_METAL(
+      Stage.AFTER_TEXTURE,
+      Icon.texture(Util.newThemeLocation("overlay/round_simple_metal")),
+      (context, radius) -> {
+        context.canvas().submitCircle(
+            radius, radius,
+            radius + 1,
+            Integer.MAX_VALUE
+        );
+      },
+      true
+  ),
+  ROUND_WOOD(
+      Stage.AFTER_TEXTURE,
+      Icon.texture(Util.newThemeLocation("overlay/round_wood")),
+      (context, radius) -> {
+        context.canvas().submitCircle(
+            radius, radius,
+            radius - Util.BORDER_PADDING,
+            Integer.MAX_VALUE
+        );
+      },
+      true
+  ),
   SQUARE(
       Stage.AFTER_TEXTURE,
       Icon.texture(Util.newThemeLocation("overlay/square")),
-      (context, radius) -> {
+      (context, radius) ->
+      {
+        float size = radius * 2F - Util.BORDER_PADDING * 2F;
+        context.canvas().submitRelativeRect(
+            Util.BORDER_PADDING, Util.BORDER_PADDING,
+            size, size,
+            Integer.MAX_VALUE
+        );
+      }
+  ),
+  SQUARE_PARCHMENT(
+      Stage.AFTER_TEXTURE,
+      Icon.texture(Util.newThemeLocation("overlay/square_parchment")),
+      (context, radius) ->
+      {
         float size = radius * 2F - Util.BORDER_PADDING * 2F;
         context.canvas().submitRelativeRect(
             Util.BORDER_PADDING, Util.BORDER_PADDING,
@@ -58,7 +134,8 @@ public enum MinimapDisplayType {
   MINECRAFT_MAP_SQUARE(
       Stage.BEFORE_TEXTURE,
       Icon.texture(ResourceLocation.create("minecraft", "textures/map/map_background.png")),
-      (context, radius) -> {
+      (context, radius) ->
+      {
         float size = radius * 2F - Util.BORDER_PADDING * 2F;
         context.canvas().submitRelativeRect(
             Util.BORDER_PADDING, Util.BORDER_PADDING,
