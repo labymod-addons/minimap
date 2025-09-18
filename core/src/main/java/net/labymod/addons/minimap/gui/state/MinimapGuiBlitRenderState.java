d3fcb817a839f1e7b3e33f1f8287d793c2edccf1
Christian <r0bby@gmx.net>
2025-09-18 14:49:42 +0200

    Update `MinimapGuiBlitRenderState` to use `DrawCommandContext` for improved command context handling.

--- core/src/main/java/net/labymod/addons/minimap/gui/state/MinimapGuiBlitRenderState.java
+++ core/src/main/java/net/labymod/addons/minimap/gui/state/MinimapGuiBlitRenderState.java
@@ -2,11 +2,11 @@ package net.labymod.addons.minimap.gui.state;
 
 import net.labymod.addons.minimap.laby3d.MinimapUniformBlocks;
 import net.labymod.addons.minimap.laby3d.shader.MinimapUniformBlock;
+import net.labymod.api.client.gui.screen.state.DrawCommandContext;
 import net.labymod.api.client.gui.screen.state.states.GuiBlitRenderState;
 import net.labymod.api.client.gui.screen.state.states.GuiTextureSet;
 import net.labymod.api.client.gui.screen.util.scissor.ScissorArea;
 import net.labymod.laby3d.api.pipeline.RenderState;
-import net.labymod.laby3d.api.pipeline.pass.DrawRenderCommand;
 import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
 
@@ -42,8 +42,8 @@ public class MinimapGuiBlitRenderState extends GuiBlitRenderState {
   }
 
   @Override
-  public void consumeCommand(DrawRenderCommand command) {
-    command.setUniformBlock(MinimapUniformBlock.NAME, this.uniformBlocks.minimap());
+  public void consumeCommand(DrawCommandContext command) {
+    command.drawRenderCommand().setUniformBlock(MinimapUniformBlock.NAME, this.uniformBlocks.minimap());
     super.consumeCommand(command);
   }
 }
