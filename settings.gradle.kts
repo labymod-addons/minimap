3efc6f20adf85d2d24490069e62ecd6ece32b47e
Christian <r0bby@gmx.net>
2026-06-03 13:58:58 +0200

    Update LabyGradle to 0.8.0, sync CI workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "minimap"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.6.1"
+    val labyGradlePluginVersion = "0.8.0"
     buildscript {
         repositories {
             maven("https://dist.labymod.net/api/v1/maven/release/")
